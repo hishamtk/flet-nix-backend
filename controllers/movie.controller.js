11fcdbf650fc860d8515724aed01f39769ef1388
hishamtk <tkhisham007@gmail.com>
2023-04-04 16:19:41 +0530

    date added

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -12,31 +12,72 @@ let getAllMovieController = async (req, res) => {
   try {
     let user = req.user;
 
-    const { page = 1, limit = 15, search, sort, order , type } = req.query;
+    const {
+      page = 1,
+      limit = 15,
+      search,
+      sort = title,
+      order = "asc",
+      type,
+    } = req.query;
 
-    let query = {};
-    if(user.age < 18){
-      query.rating = {$ne : "R"}
-    }
-    if(type){
-      query.type = type;
-    }
-    if(search){
-      query.$or=[
-        { title: new RegExp(search, "i") },
-        { cast: new RegExp(search, "i") },
-      ]
+    let query = [
+      {
+        $project: {
+          date_added: {
+            $dateFromString: {
+               $toDate: '$date_added'
+            }
+         },
+          show_id: 1,
+          cast: 1,
+          title: 1,
+          type: 1,
+          rating: 1,
+        },
+      },
+    ];
+
+    if (search) {
+      query.push({
+        $match: {
+          $or: [
+            { title: new RegExp(search, "i") },
+            { cast: new RegExp(search, "i") },
+          ],
+        },
+      });
     }
 
+    if (type) {
+      query.push({
+        $match: {
+          type: type,
+        },
+      });
+    }
 
-    let movies = await Movie.find(query)
-      .select("_id show_id cast title type rating")
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .sort([[sort,order]]);
+    if (user.age < 18) {
+      query.push({
+        $match: {
+          rating: { $ne: "R" },
+        },
+      });
+    }
+
+    const count = await Movie.countDocuments(query);
+    let skip = (page - 1) * limit;
+
+    query.push({ $skip: skip });
+    query.push({ $limit: limit });
+    if (sort && order) {
+      let sortObje = {};
+      sortObje[sort] = order == "asc" ? 1 : -1;
+      query.push({ $sort: sortObje });
+    }
+    let movies = await Movie.aggregate(query);
 
     // get total documents in the Movie collection
-    const count = await Movie.countDocuments(query);
     return res.status(200).json({
       movies,
       totalPages: Math.ceil(count / limit),
@@ -51,7 +92,7 @@ let getAllMovieController = async (req, res) => {
 
 let getOneMovieController = async (req, res) => {
   try {
-    let {id} = req.params;
+    let { id } = req.params;
     let movie = await Movie.find({ _id: id });
 
     return res.status(200).json({
